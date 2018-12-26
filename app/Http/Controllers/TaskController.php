f2de719e33f22e28511ac7d00e88da7e49d60575
Vasyl <admin@gmail.com>
2018-12-26 17:56:04 +0200

    change

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Task;
 
 class TaskController extends Controller{
 
@@ -15,6 +16,43 @@ class TaskController extends Controller{
      */
     public function __construct() {
 	$this->middleware('auth');
+    }
+
+    /**
+     * Отображение списка всех задач пользователя.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request) {
+	$tasks = $request->user()->tasks;
+	return view('tasks.index',[
+	    'tasks'=>$tasks,
+	]);
+    }
+
+    /**
+     * Создание новой задачи.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request) {
+	$this->validate($request, [
+	    'name' => 'required|max:255',
+	]);
+
+	$request->user()->tasks()->create([
+	    'name' => $request->name,
+	]);
+
+	return redirect('/tasks');
+    }
+    
+    public function destroy(Task $task, Request $request){
+	$this->authorize('destroy', $task);
+	$task->delete();
+	return redirect('/tasks');
     }
 
 }
